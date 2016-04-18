4d49d8fc1835d5c1ce315c29142bbfaf52433031
Johannes Deck <jodeck@htwg-konstanz.de>
2016-04-18 11:55:11 +0200

    added fileout, finished argv

--- v2.c
+++ v2.c
@@ -6,6 +6,7 @@
 
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/times.h>
 #include <unistd.h>
 #include <time.h>
@@ -44,9 +45,11 @@ main (int argc, char *argv[])
 {
 
 	char * filename;
-	unsigned long min, max, step;
+	FILE * fp;
+	long min, max, step;
 	struct timespec req, rem, start, end, diff;
 	int error;
+	int rtflag = 0;
 	long max_delay, delay;
 	int i,j;
 
@@ -57,49 +60,50 @@ main (int argc, char *argv[])
 		return 0;
 	}
 
+
 	for(i = 1; i < argc; i++)
-	{
-		printf("%s,\n", argv[i]);
-/*
+	{	
+
 		if (strcmp("-min",argv[i]) == 0)
 		{
-			min = strtoul(argv[i+1]);
+			min = atol(argv[i+1]);
 			
 		}
 		else if (strcmp("-max",argv[i]) == 0)
 		{
-			max = strtoul(argv[i+1]);
+			max = atol(argv[i+1]);
 		}
 		else if (strcmp("-step",argv[i]) == 0)
 		{
-			step = strtoul(argv[i+1]);
+			step = atol(argv[i+1]);
 		}
 		else if (strcmp("-out",argv[i]) == 0)
 		{
 			filename = argv[i+1];
 		}
-		printf("%ld %ld %ld \n", min, max, step);
-*/
-
-
-
+		else if (strcmp("-out",argv[i]) == 0)
+		{
+			rtflag = 1;
+		}
+		
 	}
+	//printf("%ld %ld %ld %s \n", min, max, step, filename);
 
-return 0;
+
 	req.tv_sec = 0;
 	max_delay = 0;
-	double fors = (max-min) / step;
+	int fors = ((max-min) / step)+1;
 	//long fors = 3;
 //printf("%ld\n", fors);
 	
-
-	printf("Starting measurment with period[us]: 10000 f0r 100 loops:\n");
-
+	fp = fopen(filename, "w+");
 	for (j=0; j < fors; j++)
 	{
-		req.tv_nsec = min*1000+(j*step);
+		req.tv_nsec = (min+j*step)*1000;
+
+		//printf("Starting measurment with period[us]: %d f0r 100 loops:\n", req.tv_nsec/1000);
 		
-		for (i=0; i<10; i++)
+		for (i=0; i<100; i++)
 		{
 			clock_gettime(CLOCK_MONOTONIC, &start);
 			if ((error=clock_nanosleep(CLOCK_MONOTONIC, 0, &req, NULL))!=0)
@@ -109,16 +113,22 @@ return 0;
 			clock_gettime(CLOCK_MONOTONIC, &end);
 			diff_time(start, end, &diff);
 			delay = (diff.tv_nsec-req.tv_nsec)/1000;
-			printf("value:%ld usec, delay: %ld usec, raw_data: %ld\n", diff.tv_nsec/1000, delay, diff.tv_nsec);
+			//printf("value:%ld usec, delay: %ld usec, raw_data: %ld\n", diff.tv_nsec/1000, delay, diff.tv_nsec);
 			if (delay > max_delay)
 			{
 				max_delay = delay;
 			}
 		}
-		printf("-------------------\n");
+		//printf("Max: %ld usec\n", max_delay);
+		//printf("-------------------\n");
+		fprintf(fp, "%lu %lu\n", req.tv_nsec/1000, max_delay);
+		fprintf(stdout, "%lu %lu\n", req.tv_nsec/1000, max_delay);
+		max_delay = 0;
 	}
 
-		printf("Max: %ld usec\n", max_delay);
+	fclose(fp);
+
+		
     
 
 }
