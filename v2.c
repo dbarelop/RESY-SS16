85e51d4828ac7479ead5c58376fcb312e5dc7e19
Johannes Deck <jodeck@htwg-konstanz.de>
2016-04-18 12:24:51 +0200

    threadrunning (start)

--- v2.c
+++ v2.c
@@ -38,6 +38,77 @@ struct timespec * diff_time(struct timespec before, struct timespec after, struc
 	return result;
 }
 
+typedef struct params {
+	long min, max, step;
+	char filename;
+} params_t;
+
+
+void sleep_function(void * arg)
+{
+	params_t params = *((params_t *) arg);
+	long min = params.min;
+	long max = params.max;
+	long step = params.step;
+	char * filename = params.filename;
+	FILE * fp;
+
+
+
+
+
+
+
+
+	req.tv_sec = 0;
+	max_delay = 0;
+	int fors = ((max-min) / step)+1;
+	//long fors = 3;
+//printf("%ld\n", fors);
+	
+	fp = fopen(filename, "w+");
+	for (j=0; j < fors; j++)
+	{
+		req.tv_nsec = (min+j*step)*1000;
+
+		//printf("Starting measurment with period[us]: %d f0r 100 loops:\n", req.tv_nsec/1000);
+		
+		for (i=0; i<100; i++)
+		{
+			clock_gettime(CLOCK_MONOTONIC, &start);
+			if ((error=clock_nanosleep(CLOCK_MONOTONIC, 0, &req, NULL))!=0)
+			{
+				printf("clock_nanosleep reporting error %d\n",error);
+			}
+			clock_gettime(CLOCK_MONOTONIC, &end);
+			diff_time(start, end, &diff);
+			delay = (diff.tv_nsec-req.tv_nsec)/1000;
+			//printf("value:%ld usec, delay: %ld usec, raw_data: %ld\n", diff.tv_nsec/1000, delay, diff.tv_nsec);
+			if (delay > max_delay)
+			{
+				max_delay = delay;
+			}
+		}
+		//printf("Max: %ld usec\n", max_delay);
+		//printf("-------------------\n");
+		fprintf(fp, "%lu %lu\n", req.tv_nsec/1000, max_delay);
+		fprintf(stdout, "%lu %lu\n", req.tv_nsec/1000, max_delay);
+		max_delay = 0;
+	}
+
+	fclose(fp);
+
+
+
+
+
+
+
+
+
+
+}
+
 
 
 int
@@ -90,44 +161,7 @@ main (int argc, char *argv[])
 	//printf("%ld %ld %ld %s \n", min, max, step, filename);
 
 
-	req.tv_sec = 0;
-	max_delay = 0;
-	int fors = ((max-min) / step)+1;
-	//long fors = 3;
-//printf("%ld\n", fors);
 	
-	fp = fopen(filename, "w+");
-	for (j=0; j < fors; j++)
-	{
-		req.tv_nsec = (min+j*step)*1000;
-
-		//printf("Starting measurment with period[us]: %d f0r 100 loops:\n", req.tv_nsec/1000);
-		
-		for (i=0; i<100; i++)
-		{
-			clock_gettime(CLOCK_MONOTONIC, &start);
-			if ((error=clock_nanosleep(CLOCK_MONOTONIC, 0, &req, NULL))!=0)
-			{
-				printf("clock_nanosleep reporting error %d\n",error);
-			}
-			clock_gettime(CLOCK_MONOTONIC, &end);
-			diff_time(start, end, &diff);
-			delay = (diff.tv_nsec-req.tv_nsec)/1000;
-			//printf("value:%ld usec, delay: %ld usec, raw_data: %ld\n", diff.tv_nsec/1000, delay, diff.tv_nsec);
-			if (delay > max_delay)
-			{
-				max_delay = delay;
-			}
-		}
-		//printf("Max: %ld usec\n", max_delay);
-		//printf("-------------------\n");
-		fprintf(fp, "%lu %lu\n", req.tv_nsec/1000, max_delay);
-		fprintf(stdout, "%lu %lu\n", req.tv_nsec/1000, max_delay);
-		max_delay = 0;
-	}
-
-	fclose(fp);
-
 		
     
 
